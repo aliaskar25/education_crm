d19301ea84334662026d370fb89ae587c97561e6
aliaskar25 <aliaskar.isakov@gmail.com>
2023-02-02 11:54:00 +0600

    add tests

--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
     default="postgresql+asyncpg://education_crm_user:education_crm_password@0.0.0.0:5432/education_crm_db"
 )
+
+
+TEST_DATABASE_URL = env.str(
+    "TEST_DATABASE_URL",
+    default="postgresql+asyncpg://education_crm_test_user:education_crm_test_password@0.0.0.0:5432/education_crm_test_db"
+)
